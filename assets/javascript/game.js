48e40b07ebaeb6aaed265a96bd076d1fb5b72aca
Michael Anderson <MacTerrier0@michaels-mbp.attlocal.net>
2019-03-10 03:04:31 -0700

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,48 +2,58 @@ let picObj = [
     catwoman = {
         name: 'Cat Woman',
         hp: 100,
-        id: '#heros',
-        yourCharacter: false,
-        defender: false,
-        charList: true,
+        id: 'heros',
+        picClicked: false,
+        // yourCharacter: true,
+        // defender: false,
+        // charList: true,
         pic: './assets/images/thumbnails/catwoman.png'
     },
     drdoom = {
         name: 'Dr. Doom',
         hp: 100,
-        id: '#heros',
-        yourCharacter: false,
-        defender: false,
-        charList: true,
+        id: 'heros',
+        picClicked: false,
+        // yourCharacter: false,
+        // defender: false,
+        // charList: true,
         pic: './assets/images/thumbnails/drDoom.png'
     },
     magneto = {
         name: 'Magneto',
         hp: 100,
-        id: '#heros',
-        yourCharacter: false,
-        defender: false,
-        charList: true,
+        id: 'heros',
+        picClicked: false,
+        // yourCharacter: false,
+        // defender: false,
+        // charList: true,
         pic: './assets/images/thumbnails/magneto.png'
     },
     joker = {
         name: 'Joker',
         hp: 100,
-        id: '#heros',
-        yourCharacter: false,
-        defender: false,
-        charList: true,
+        id: 'heros',
+        picClicked: false,
+        // yourCharacter: false,
+        // defender: false,
+        // charList: true,
         pic: './assets/images/thumbnails/joker.png'
     }
 ]
 
+const init = _ => {
+}
+
+
+
 for (let i = 0; i < picObj.length; i++) {
+    const heroEl = document.createElement('div')
 
     const heroCard = `
     <div class="col l2">
         <div class="card">
             <div class="card-image">
-                <img data-value="${picObj[i].charList}" src="${picObj[i].pic}">
+                <img class="${picObj[i].id}" data-name="${picObj[i]}" data-picclicked="${picObj[i].picClicked}" src="${picObj[i].pic}">
             </div>
             <div class="card-content">
                 <span class="card-title">${picObj[i].name}</span>
@@ -52,16 +62,14 @@ for (let i = 0; i < picObj.length; i++) {
         </div>
     </div>
     `
-    const heros = `${picObj[i].id}`
-    const heroEl = document.createElement('div')
 
     heroEl.innerHTML = heroCard;
-    document.querySelector(heros).append(heroEl)
+    document.querySelector(`#${picObj[i].id}`).append(heroEl)
 }
 
 document.addEventListener('click', e => {
-    console.log(e.target.dataset.value);
+    console.log(e.target.dataset.name);
+
 
 
 })
-
