2829494636f95f0ec4881428f0ad3331a9f5cf42
Michael Anderson <MacTerrier0@michaels-mbp.attlocal.net>
2019-03-10 22:57:16 -0700

    updated event listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,93 @@
-let picObj = [
-    catwoman = {
+let heroSelected, defenderSelected
+
+
+let picObj = {
+    catwoman: {
+        tag: 'catwoman',
         name: 'Cat Woman',
         hp: 100,
         id: 'heros',
         picClicked: false,
-        // yourCharacter: true,
-        // defender: false,
-        // charList: true,
+        status: 'waiting',
         pic: './assets/images/thumbnails/catwoman.png'
     },
-    drdoom = {
+    drdoom: {
+        tag: 'drdoom',
         name: 'Dr. Doom',
         hp: 100,
         id: 'heros',
         picClicked: false,
-        // yourCharacter: false,
-        // defender: false,
-        // charList: true,
+        status: 'waiting',
         pic: './assets/images/thumbnails/drDoom.png'
     },
-    magneto = {
+    magneto: {
+        tag: 'magneto',
         name: 'Magneto',
         hp: 100,
         id: 'heros',
         picClicked: false,
-        // yourCharacter: false,
-        // defender: false,
-        // charList: true,
+        status: 'waiting',
         pic: './assets/images/thumbnails/magneto.png'
     },
-    joker = {
+    joker: {
+        tag: 'joker',
         name: 'Joker',
         hp: 100,
         id: 'heros',
         picClicked: false,
-        // yourCharacter: false,
-        // defender: false,
-        // charList: true,
+        status: 'waiting',
         pic: './assets/images/thumbnails/joker.png'
     }
-]
-
-const init = _ => {
 }
 
+const init = _ => {
+document.getElementById('heros').innerHTML = ''
+document.getElementById('mySelect').innerHTML = ''
+document.getElementById('defender').innerHTML = ''
 
+}
 
-for (let i = 0; i < picObj.length; i++) {
-    const heroEl = document.createElement('div')
+const picElem = (picObj) => {
+    for (hero in picObj) {
+        const heroEl = document.createElement('div')
 
-    const heroCard = `
-    <div class="col l2">
-        <div class="card">
-            <div class="card-image">
-                <img class="${picObj[i].id}" data-name="${picObj[i]}" data-picclicked="${picObj[i].picClicked}" src="${picObj[i].pic}">
-            </div>
-            <div class="card-content">
-                <span class="card-title">${picObj[i].name}</span>
-                <p>HP's: ${picObj[i].hp}</p>
+        const heroCard = `
+        <div class="col l2">
+            <div class="card">
+                <div class="card-image">
+                    <img id="${picObj[hero].id}-${hero}" name="${picObj[hero].name}" data-picclicked="${picObj[hero].picClicked}" data-tag="${picObj[hero].tag}" src="${picObj[hero].pic}">
+                </div>
+                <div class="card-content">
+                    <span class="card-title">${picObj[hero].name}</span>
+                    <p>HP's: ${picObj[hero].hp}</p>
+                </div>
             </div>
         </div>
-    </div>
-    `
+        `
 
-    heroEl.innerHTML = heroCard;
-    document.querySelector(`#${picObj[i].id}`).append(heroEl)
+        heroEl.innerHTML = heroCard;
+        document.querySelector(`#${picObj[hero].id}`).append(heroEl)
+    }
 }
 
 document.addEventListener('click', e => {
-    console.log(e.target.dataset.name);
+    console.log(e);
+    console.log(e.target);
+
+    let idChange = document.getElementById(e.target.id)
+    let objFind = idChange.dataset.tag
+    console.log(picObj[objFind].name);
+
+    if (picObj[objFind].picClicked === false && picObj[objFind].id === 'heros') {
+        picObj[objFind].picClicked = true
+        picObj[objFind].id = 'defender'
+        picObj[objFind].status = 'fighting'
+        init()
+        picElem(picObj)
+    }
 
 
 
 })
+
+picElem(picObj)
